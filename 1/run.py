fd48ecc65f80b9dd7d803b4cfcaa871d5c37970a
Nathan-Binkley <nbink2@gmail.com>
2022-11-30 23:28:07 -0500

    day 1

---
+++ 1/run.py
@@ -0,0 +1,18 @@
+lines = []
+with open("input.txt", "r") as f:
+    l = f.readline()
+    while l:
+        lines.append(int(l.rstrip()))
+        l = f.readline()
+
+
+def part1():
+    pass
+
+print("Part 1: ", part1())
+
+def part2():
+    pass
+
+print("Part 2: ", part2())
+
